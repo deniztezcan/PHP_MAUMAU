e1c0a1ece8e986afd79ded3d14309e063f516daa
Deniz Tezcan <d.tezcan42@gmail.com>
2020-05-07 14:58:57 +0000

    Apply fixes from StyleCI

--- src/Game.php
+++ src/Game.php
@@ -201,7 +201,6 @@ class Game
 
                         return;
                     } elseif ($this->finished) {
-                        
                         return;
                     }
                 }
